9ce7a2a37eb76d4ef37e22f04554bdb7d1ccb718
Abiram.Arana <aranagomez12@hotmail.com>
2019-08-09 15:17:17 -0500

    tarea2

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StoreModule } from './store/store.module';
+import { HttpClientModule } from '@angular/common/http';
+import { NavComponent } from './store/nav/nav.component';
+import { CartSummaryComponent } from './store/cart-summary/cart-summary.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    CartSummaryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
